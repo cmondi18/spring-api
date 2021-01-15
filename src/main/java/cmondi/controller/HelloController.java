558b3e69982db191219c13ca9ad0ac0aa2119427
Денис Сиволап <denissivolap@MBP-Denis.Dlink>
2021-01-15 13:07:21 +0700

    fix: change logging to filter

--- src/main/java/cmondi/controller/HelloController.java
+++ src/main/java/cmondi/controller/HelloController.java
@@ -1,8 +1,6 @@
 package cmondi.controller;
 
 import cmondi.model.IpifyResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,7 +10,6 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 @RequestMapping("api")
 public class HelloController {
-    Logger logger = LoggerFactory.getLogger(HelloController.class);
 
     private String URL = "https://api.ipify.org?format=json";
 
@@ -21,12 +18,8 @@ public class HelloController {
 
     @GetMapping(path = "/hello", produces = "application/json")
     public IpifyResponse hello() {
-        logger.info("Starting use /api/hello");
-        logger.info("Trying to get ip from server");
         IpifyResponse ipify = restTemplate.getForObject(URL, IpifyResponse.class);
         ipify.setLang("java");
-        logger.info("Successfully get one, put our information");
-        logger.info("Return the new data");
         return ipify;
     }
 }
